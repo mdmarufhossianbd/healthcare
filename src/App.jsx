cbe22d92ee859103e7c27dd79cd67b7ff6649d4c
Maruf Hossain <mdmarufh974@gmail.com>
2024-07-03 20:24:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navber from './components/Navber'
+
+function App() {
+
+  return (
+    <>
+      <Navber></Navber>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navber.jsx
@@ -0,0 +1,9 @@
+const Navber = () => {
+    return (
+        <div>
+            <h2>This is Navber</h2>
+        </div>
+    );
+};
+
+export default Navber;
